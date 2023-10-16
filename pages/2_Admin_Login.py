cc3f93e510168afbc076329be0fd4da2482238cf
LionRion <ghodoussirion@gmail.com>
2023-10-15 19:21:14 -0500

    commit

--- pages/2_Admin_Login.py
+++ pages/2_Admin_Login.py
@@ -6,7 +6,7 @@ def check_password():
 
     def password_entered():
         """Checks whether a password entered by the user is correct."""
-        if hmac.compare_digest(st.session_state["password"], st.secrets["db_password"]):
+        if hmac.compare_digest(st.session_state["password"], st.secrets["pwd"]):
             st.session_state["password_correct"] = True
             del st.session_state["password"]  # Don't store the password.
         else:
